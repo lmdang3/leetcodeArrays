eeec4c5075bb980faa5703e2ba5659df84caa272
lmdang3 <89552436+lmdang3@users.noreply.github.com>
2023-02-21 13:02:17 -0600

    Create leetcode35.py

---
+++ leetcode35.py
@@ -0,0 +1,12 @@
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+
+      
+        # for i in enumerate nums
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums.append(target)
+            nums.sort()
+            return nums.index(target)
+        
